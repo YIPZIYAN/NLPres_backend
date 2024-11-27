a5040e193d7e3589aa633dec9196717a2a7affd5
GohChunYen <chunyengoh030503@gmail.com>
2024-11-27 15:56:28 +0800

    CSV

--- document/serializers.py
+++ document/serializers.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from functools import partial
 
@@ -26,78 +27,6 @@ class ImportDocumentSerializer(serializers.Serializer):
     project_id = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
     key = serializers.CharField(required=False, allow_null=True)
 
-    def create_document(self, project, lines):
-        documents = [Document(project=project, text=line) for line in lines]
-        Document.objects.bulk_create(documents)
-        return documents
-
-    def process_txt(self, file, project):
-
-        lines = []
-        errors = []
-
-        for line_number, line in enumerate(file, start=1):
-            try:
-                decoded_line = line.decode('utf-8').strip()
-                if decoded_line:
-                    lines.append(decoded_line)
-
-            except UnicodeDecodeError as e:
-                errors.append({file.name: f"{str(e)} in line {line_number}."})
-
-        if not lines:
-            errors.append({file.name: "No data found."})
-
-        return self.create_document(project, lines), errors
-
-    def process_json(self, file, project, key):
-
-        errors = []
-        lines = []
-
-        try:
-            data = json.load(file)
-            if isinstance(data, list):
-                lines = [
-                    item.get(key) for item in data
-                    if isinstance(item, dict) and item.get(key) is not None
-                ]
-            elif isinstance(data, dict):
-                lines = [data.get(key)] if data.get(key) is not None else []
-            else:
-                errors.append({file.name: "JSON must be an object or an array of objects."})
-
-            if not lines:
-                errors.append({file.name: f"No data found for the key '{key}' in the JSON file(s)."})
-
-        except json.JSONDecodeError as e:
-            errors.append({file.name: f"{str(e)}."})
-
-        return self.create_document(project, lines), errors
-
-    def process_jsonl(self, file, project, key):
-
-        lines = []
-        errors = []
-
-        for line_number, line in enumerate(file, start=1):
-            try:
-                data = json.loads(line.decode('utf-8'))
-                if isinstance(data, dict):
-                    value = data.get(key)
-                    if value is not None:
-                        lines.append(value)
-                    else:
-                        errors.append({file.name: f"No data found for the key '{key}' in line {line_number}."})
-
-                else:
-                    errors.append({file.name: f"Invalid JSON object in line {line_number}."})
-
-            except json.JSONDecodeError as e:
-                errors.append({file.name: f"{str(e)} in line {line_number}."})
-
-        return self.create_document(project, lines), errors
-
     def create(self, validated_data):
         files = validated_data['files']
         project = validated_data['project_id']
@@ -108,7 +37,8 @@ class ImportDocumentSerializer(serializers.Serializer):
         file_formats = {
             '.txt': self.process_txt,
             '.json': partial(self.process_json, key=key),
-            '.jsonl': partial(self.process_jsonl, key=key)
+            '.jsonl': partial(self.process_jsonl, key=key),
+            '.csv': partial(self.process_csv, key=key)
         }
 
         for file in files:
@@ -131,7 +61,7 @@ class ImportDocumentSerializer(serializers.Serializer):
             message = f"{len(created_documents)} data imported successfully from {len(files)} file(s)."
             response = {
                 "message": message,
-                "created_documents": [
+                "objects": [
                     {"id": doc.id, "text": doc.text} for doc in created_documents
                 ],
                 "errors": errors
@@ -145,3 +75,103 @@ class ImportDocumentSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         return instance
+
+    def process_txt(self, file, project):
+
+        lines = []
+        errors = []
+
+        try:
+            for line_number, line in enumerate(file, start=1):
+                decoded_line = line.decode('utf-8').strip()
+                if decoded_line:
+                    lines.append(decoded_line)
+
+            if not lines:
+                errors.append({file.name: "No data found"})
+
+        except UnicodeDecodeError as e:
+            errors.append({file.name: f"{str(e)}"})
+
+        return self.create_document(project, lines), errors
+
+    def process_json(self, file, project, key):
+
+        errors = []
+        lines = []
+
+        try:
+            data = json.load(file)
+            if isinstance(data, list):
+                for line_number, item in enumerate(data, start=1):
+                    if isinstance(item, dict) and item.get(key) is not None:
+                        lines.append(item.get(key))
+                    else:
+                        errors.append({file.name: f"Line {line_number}: No data found for the key '{key}'"})
+            elif isinstance(data, dict):
+                value = data.get(key)
+                if value is not None:
+                    lines.append(value)
+                else:
+                    errors.append({file.name: f"No data found for the key '{key}'"})
+
+            else:
+                errors.append({file.name: "JSON must be an object or an array of objects"})
+
+        except json.JSONDecodeError as e:
+            errors.append({file.name: f"{str(e)}"})
+
+        return self.create_document(project, lines), errors
+
+    def process_jsonl(self, file, project, key):
+
+        lines = []
+        errors = []
+
+        for line_number, line in enumerate(file, start=1):
+            try:
+                data = json.loads(line.decode('utf-8'))
+                if isinstance(data, dict):
+                    value = data.get(key)
+                    if value is not None:
+                        lines.append(value)
+                    else:
+                        errors.append({file.name: f"Line {line_number}: No data found for the key '{key}'"})
+
+                else:
+                    errors.append({file.name: f"Line {line_number}: Invalid JSON object"})
+
+            except json.JSONDecodeError as e:
+                errors.append({file.name: f"Line {line_number}: {str(e)}"})
+
+        return self.create_document(project, lines), errors
+
+    def process_csv(self, file, project, key):
+
+        lines = []
+        errors = []
+
+        try:
+            data = file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(data)
+
+            if key not in reader.fieldnames:
+                errors.append({file.name: f"Key '{key}' not found in CSV header"})
+                return [], errors
+
+            for line_number, row in enumerate(reader, start=2):
+                value = row.get(key)
+                if value:
+                    lines.append(value)
+                else:
+                    errors.append({file.name: f"Line {line_number}: Invalid data"})
+
+        except csv.Error as e:
+            errors.append({file.name: f"{str(e)}"})
+
+        return self.create_document(project, lines), errors
+
+    def create_document(self, project, lines):
+        documents = [Document(project=project, text=line) for line in lines]
+        Document.objects.bulk_create(documents)
+        return documents
