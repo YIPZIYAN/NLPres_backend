d29cce7b43d08a4214bf665c087f363aae3e4369
GohChunYen <chunyengoh030503@gmail.com>
2024-12-01 19:49:10 +0800

    Update serializers.py

--- converter/serializers.py
+++ converter/serializers.py
@@ -1,7 +1,7 @@
 import csv
 import json
 import zipfile
-from io import BytesIO
+from io import BytesIO, TextIOWrapper, StringIO
 
 from rest_framework import serializers
 
@@ -10,8 +10,8 @@ class ConverterSerializer(serializers.Serializer):
     files = serializers.ListField(
         child=serializers.FileField(),
     )
-    file_format = serializers.ChoiceField(choices=['txt', 'jsonl', 'csv'])
-    export_as = serializers.ChoiceField(choices=['json', 'jsonl', 'csv', 'conllu'])
+    file_format = serializers.ChoiceField(choices=['txt', 'json', 'jsonl', 'csv'])
+    export_as = serializers.ChoiceField(choices=['txt', 'json', 'jsonl', 'csv'])
 
     def save(self):
         files = self.validated_data['files']
@@ -24,8 +24,12 @@ class ConverterSerializer(serializers.Serializer):
 
         return process_method(files, export_as)
 
+    # File Processors
     def process_txt(self, files, export_as):
         return self.process_files(files, export_as, file_reader=self.read_txt)
+
+    def process_json(self, files, export_as):
+        return self.process_files(files, export_as, file_reader=self.read_json)
 
     def process_jsonl(self, files, export_as):
         return self.process_files(files, export_as, file_reader=self.read_jsonl)
@@ -59,12 +63,37 @@ class ConverterSerializer(serializers.Serializer):
         content = file.read().decode('utf-8')
         return [{"text": line.strip()} for line in content.splitlines() if line.strip()]
 
+    def read_json(self, file):
+        return json.load(file)
+
     def read_jsonl(self, file):
         return [json.loads(line) for line in file.read().decode('utf-8').splitlines()]
 
     def read_csv(self, file):
         return list(csv.DictReader(file.read().decode('utf-8').splitlines()))
 
-    # Exporters
+    # File Exporters
     def to_json(self, content):
         return json.dumps(content, indent=2).encode('utf-8')
+
+    def to_jsonl(self, content):
+        return "\n".join(json.dumps(item) for item in content).encode('utf-8')
+
+    def to_csv(self, content):
+        flattened_content = []
+        for item in content:
+            flattened_item = {}
+            for key, value in item.items():
+                if isinstance(value, list):
+                    flattened_item[key] = ", ".join(value)
+                else:
+                    flattened_item[key] = value
+            flattened_content.append(flattened_item)
+
+        output = StringIO()
+        fieldnames = flattened_content[0].keys() if flattened_content else []
+        writer = csv.DictWriter(output, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(flattened_content)
+
+        return output.getvalue().encode('utf-8')
