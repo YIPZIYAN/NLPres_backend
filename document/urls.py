cc67fc9fc798ff5a5efdb731997167916f41fc80
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-12-16 02:19:29 +0800

    add progress and pagination

--- document/urls.py
+++ document/urls.py
@@ -3,6 +3,8 @@ from document import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pagination', views.pagination, name='pagination'),
+    path('progress', views.progress, name='progress'),
     path('create', views.create, name='create'),
     path('<int:document_id>',views.document_details,name='document_details'),
     path('export', views.export, name='export')
