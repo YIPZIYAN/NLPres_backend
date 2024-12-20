9901674e75156982ef50b22f803b0cc456d0a20b
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-12-20 22:58:30 +0800

    batch create label

--- annotation/serializers.py
+++ annotation/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from document.models import Annotation, Document
 from label.models import Label
@@ -13,7 +14,7 @@ class AnnotationSerializer(serializers.ModelSerializer):
     start = serializers.IntegerField(allow_null=True, required=False)
     end = serializers.IntegerField(allow_null=True, required=False)
     label_id = serializers.IntegerField(write_only=True, required=True)
-    document_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+    document_id = serializers.IntegerField(write_only=True, required=True)
     document = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
@@ -21,19 +22,10 @@ class AnnotationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-
-        try:
-            label = Label.objects.get(id=validated_data['label_id'])
-        except Label.DoesNotExist:
-            raise serializers.ValidationError({'label_id': 'Invalid label ID'})
-
-        try:
-            document = Document.objects.get(id=validated_data['document_id'])
-        except Document.DoesNotExist:
-            raise serializers.ValidationError({'document_id': 'Invalid document ID'})
-
+        label = get_object_or_404(Label, pk=validated_data['label_id'])
+        document = get_object_or_404(Document, pk=validated_data['document_id'])
         return Annotation.objects.create(label=label, document=document, user=self.context['request'].user,
-                                         **validated_data)
+                                     **validated_data)
 
     def update(self, instance, validated_data):
 
--- annotation/urls.py
+++ annotation/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from annotation import views
 
 urlpatterns = [
-    path('', views.create, name='create'),
+    path('create', views.create, name='create'),
     path('<int:pk>', views.annotation_detail, name='annotation_detail'),
 ]
