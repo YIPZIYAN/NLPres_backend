7200c813e813f1e42f993403175bfcce963a038f
CY <111054799+GohChunYen@users.noreply.github.com>
2024-12-28 17:53:59 +0800

    comparison table

--- comparison/models.py
+++ comparison/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from project.models import Project
+
+
+class Comparison(models.Model):
+    name = models.CharField(max_length=100)
+    result = models.CharField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comparison'
