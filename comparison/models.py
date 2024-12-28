c384a4435d34e0e1dc0e668d470eb4bd0c3e89b1
CY <111054799+GohChunYen@users.noreply.github.com>
2024-12-29 00:12:37 +0800

    model comparison

--- comparison/models.py
+++ comparison/models.py
@@ -5,7 +5,8 @@ from project.models import Project
 
 class Comparison(models.Model):
     name = models.CharField(max_length=100)
-    result = models.CharField()
+    models_name = models.JSONField()
+    result = models.JSONField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
