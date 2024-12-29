e097282087ef92325b483a0bfe94c8a32138bb8e
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-12-29 23:39:48 +0800

    permission readonly

--- project/views.py
+++ project/views.py
@@ -1,19 +1,15 @@
-from dj_rest_auth.serializers import UserDetailsSerializer
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from NLPres_backend.permissions.IsProjectOwnerOrReadOnly import IsProjectOwnerOrReadOnly
 from NLPres_backend.permissions.IsProjectCollaborator import IsProjectCollaborator
 from NLPres_backend.util import calculate_progress
 from document.models import Document
 from project.models import Project, Collaborator
 from project.serializers import ProjectSerializer, CollaboratorSerializer
-from userprofile.models import CustomUser
-from userprofile.serializers import UserSerializer
 
 
 @api_view(['GET'])
@@ -34,7 +30,7 @@ def create(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsAuthenticated,IsProjectCollaborator])
+@permission_classes([IsAuthenticated, IsProjectOwnerOrReadOnly])
 def project_details(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
 
@@ -42,7 +38,7 @@ def project_details(request, project_id):
         return Response(ProjectSerializer(project, context={'request': request}).data)
 
     elif request.method == 'PUT':
-        serializer = ProjectSerializer(project,data=request.data)
+        serializer = ProjectSerializer(project, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -54,7 +50,7 @@ def project_details(request, project_id):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, IsProjectOwnerOrReadOnly])
 def completed_collaborators(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     total_count = Document.objects.filter(project=project).count()
@@ -70,7 +66,7 @@ def completed_collaborators(request, project_id):
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, IsProjectOwnerOrReadOnly])
 def collaborator_create(request, project_id):
     serializer = CollaboratorSerializer(data=request.data, context={'project_id': project_id})
     if serializer.is_valid():
@@ -80,10 +76,24 @@ def collaborator_create(request, project_id):
 
 
 @api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, IsProjectOwnerOrReadOnly])
 def collaborator_delete(request, project_id, collaborator_id):
     project = get_object_or_404(Project, pk=project_id)
     collaborator = get_object_or_404(Collaborator, pk=collaborator_id, project=project)
+    documents = Document.objects.filter(project_id=project_id)
+
+    for document in documents:
+        document.annotation_set.filter(user=collaborator.user).delete()
+
+    collaborator.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated, IsProjectCollaborator])
+def collaborator_quit(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    collaborator = project.collaborator_set.get(user=request.user)
     documents = Document.objects.filter(project_id=project_id)
 
     for document in documents:
